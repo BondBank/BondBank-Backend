559abbd59317d4c82fccdd9d1e189664c7f32e16
levblanc <levblanc@gmail.com>
2022-11-14 17:28:05 +0800

    Code clean up & formatting

--- contracts/BuyandRedeemBonds.sol
+++ contracts/BuyandRedeemBonds.sol
@@ -2,191 +2,116 @@
 // OpenZeppelin Contracts (last updated v4.7.0) (token/ERC1155/ERC1155.sol)
 
 pragma solidity ^0.8.1;
-
 pragma abicoder v2;
-
-
 
 import "./CreateBondandAdminRole.sol";
 import "https://github.com/aave/aave-v3-periphery/blob/7a9542963b8030885443800179c57ff8ffdac29c/contracts/misc/interfaces/IWETHGateway.sol";
-// import IPool from "@aave/core-v3/contracts/interfaces/IPool.sol";
-// import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
 import "./SimpleSwap.sol";
 import "./WETHgateway.sol";
 
+contract BuyandRedeemBonds is CreateBondandAdminRole, WETHgateway {
+    //these addresses contain both principal and the profit for each pool 
+    address private constant aWETHtokenaddress = 0x27B4692C93959048833f40702b22FE3578E77759;
 
+    SimpleSwap public Swap;
+    WETHgateway public Gateway;
 
+    mapping (address => address) public WETHgatewayAddr;
+    mapping (address => WETHgateway) public WETHgatewaycontract;
+    mapping (address => address) public Swapaddress;
+    mapping (address => SimpleSwap) public Swapcontract;
+    mapping (address => uint[]) internal bondsByBuyersAddr;
 
-contract BuyandRedeemBonds is CreateBondandAdminRole,WETHgateway {
+    event BondBought(
+        uint256 indexed bondId,
+        string indexed bondName,
+        uint256 indexed bondAmount,
+        uint256 bondBuyTime
+    );
 
+    event BondRedeemed(
+        uint256 indexed bondId,
+        string indexed bondName,
+        uint256 indexed bondAmount,
+        uint256 bondRedeemTime
+    );
 
-//these addresses contain both principal and the profit for each pool 
-address private constant aWETHtokenaddress = 0x27B4692C93959048833f40702b22FE3578E77759;
+    address public Pooladdress = 0x368EedF3f56ad10b9bC57eed4Dac65B26Bb667f6;
+    mapping(address => bool) public OnlyoneBond;
 
+    constructor() CreateBondandAdminRole("") {}
 
-SimpleSwap public Swap;
-WETHgateway public Gateway;
+    //returns bonds bought by a user
+    function getbondsByBuyersAddr(address addr) external view returns (uint[] memory){  
+        return bondsByBuyersAddr[addr];
+    }  
 
+    function buybond (uint id) external payable  {
+        require (msg.value == .001 ether, "Incorrect amount for this bond");
+        require (OnlyoneBond[msg.sender] == false, "You can only purchase one bond");
 
-
-
-mapping (address => address) public WETHgatewayAddr;
-mapping (address => WETHgateway) public WETHgatewaycontract;
-
-mapping (address => address) public Swapaddress;
-mapping (address => SimpleSwap) public Swapcontract;
-
-
-mapping (address => uint[]) internal bondsByBuyersAddr;
-event BondBought(
-  uint256 indexed bondId,
-  string indexed bondName,
-  uint256 indexed bondAmount,
-  uint256 bondBuyTime
-  );
-
-event BondRedeemed(
-  uint256 indexed bondId,
-  string indexed bondName,
-  uint256 indexed bondAmount,
-  uint256 bondRedeemTime
-);
-
-address public Pooladdress = 0x368EedF3f56ad10b9bC57eed4Dac65B26Bb667f6;
- mapping(address => bool) public OnlyoneBond;
-
-
-constructor() CreateBondandAdminRole("") {
- 
-    
-}
-//returns bonds bought by a user
-function getbondsByBuyersAddr(address addr) external view returns (uint[] memory){
-        
-  return bondsByBuyersAddr[addr];
-
-}  
-
- function buybond (uint id) external payable  {
-        
-     require (msg.value == .001 ether, "Incorrect amount for this bond");
-    require (OnlyoneBond[msg.sender] == false, "You can only purchase one bond");
-
-  
-
-     //Swap = new SimpleSwap();
-     //Gateway = new WETHgateway();
-
-
-
-        // WETHgatewayAddr[msg.sender] = address(Gateway);
-         //Swapaddress[msg.sender] = address(Swap);
-         //WETHgatewaycontract[msg.sender] = Gateway;
-        // Swapcontract[msg.sender] = Swap;
-    
-
-       
-        //mapping for chainlink automation
-       BondsinExistence[id].buyers.push(payable(msg.sender));
-
-       bondInfo[id].buyers.push(payable(msg.sender));
-       
-        
-      
-      this.depostitETH{value: .0008 ether}();
-      payable (bondInfo[id].BondManager).transfer(.0002 ether);
-
-      // SwapforWETH();
-      //swapWETHForALTcoin( 400000000000000, bondInfo[id].Altcoinswap);
-
-
-     
+        BondsinExistence[id].buyers.push(payable(msg.sender));
+        bondInfo[id].buyers.push(payable(msg.sender));
+             
+        this.depostitETH{value: .0008 ether}();
+        payable (bondInfo[id].BondManager).transfer(.0002 ether);
 
         //this line will transfer the bonds to the user 
-       _safeTransferFrom(address(this), msg.sender, id, 1, " ");
+        _safeTransferFrom(address(this), msg.sender, id, 1, " ");
 
-       OnlyoneBond[msg.sender] = true;       
-   
-    
-//added to track bonds by buyers, donot remove
-           bondsByBuyersAddr[msg.sender].push(id);
-              emit BondBought(
+        OnlyoneBond[msg.sender] = true;       
+           
+        //added to track bonds by buyers, donot remove
+        bondsByBuyersAddr[msg.sender].push(id);
+        emit BondBought(
             id,
             "bondName",
             1,
             block.timestamp
-             );
+        );
     }
 
     function Bondredemption () external {
+        uint256 totAmount = 0;
+        uint id;
 
-       //require (bondInfo[id].bondStartDate >= bondInfo[id].bondMaturityDate,"This bond has not yet expired");
-    // require (balanceOf(msg.sender,id) > 0 , " You do not have any bonds of this kind");
-      uint256 totAmount = 0;
-      uint id;
- 
-
-         for ( id = 0 ; id <= bondInfo[id].buyers.length; id++){
-             if (block.timestamp >= bondInfo[id].bondMaturityDate){
-               //logic for redemption
-
+        for ( id = 0 ; id <= bondInfo[id].buyers.length; id++){
+            if (block.timestamp >= bondInfo[id].bondMaturityDate){
+                //logic for redemption
                 this.WithdrawETH(type(uint256).max);
-              
-              payable (bondInfo[id].buyers[id]).transfer(address(this).balance/bondInfo[id].buyers.length);
-             _burn(bondInfo[id].buyers[id], id, balanceOf(bondInfo[id].buyers[id], id)); 
+                
+                payable (bondInfo[id].buyers[id]).transfer(address(this).balance/bondInfo[id].buyers.length);
 
-                 adminrole[bondInfo[id].BondManager] = false;
-             }
+                _burn(bondInfo[id].buyers[id], id, balanceOf(bondInfo[id].buyers[id], id)); 
 
-         }
+                adminrole[bondInfo[id].BondManager] = false;
+            }
+        }
 
-              
-     
-      totAmount=  address(this).balance;
-      DoesAdminExist = false; 
-      
-       numberofBondsinCirculation--;
+        totAmount=  address(this).balance;
+        DoesAdminExist = false; 
+        numberofBondsinCirculation--;
        
         emit BondBought(
             id,
             bondInfo[id].bondName,
              totAmount,
             block.timestamp
-             );
-
-              
+        ); 
      }
-      totAmount=  address(this).balance;
-      DoesAdminExist = false; 
-              adminrole[bondInfo[id].BondManager] = false;
-        emit BondBought(
-            id,
-            bondInfo[id].bondName,
-             totAmount,
-            block.timestamp
-             );
-              
-    }
+
+    //     totAmount = address(this).balance;
+    //     DoesAdminExist = false; 
+    //     adminrole[bondInfo[id].BondManager] = false;
+    //     emit BondBought(
+    //         id,
+    //         bondInfo[id].bondName,
+    //         totAmount,
+    //         block.timestamp
+    //     ); 
+    // }
 
      function returnbuyers (uint id) external view returns (address payable [] memory){
-
         return BondsinExistence[id].buyers;
       }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

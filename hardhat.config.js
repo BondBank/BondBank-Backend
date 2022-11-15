89a8eac409662de75c0e80220437212c6c6b7197
JJtheAndroid <jjoubert018@gmail.com>
2022-11-14 19:39:22 -0500

    Restored previous config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,43 +1,63 @@
-require("@nomiclabs/hardhat-waffle")
+require("@nomicfoundation/hardhat-toolbox");
+require("solidity-coverage");
+require("hardhat-contract-sizer");
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-solhint");
+require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
-require("@nomiclabs/hardhat-etherscan");
 
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
 
-
-
-// Replace this private key with your Goerli account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Beware: NEVER put real Ether into testing accounts
-
-
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    compilers: [
-      {
-        version: "0.8.10",
-        settings: {
-          evmVersion: "istanbul",
-          optimizer: {
-            enabled: true,
-            runs: 1000,
-          },
-        },
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000000,
       },
-    ],
+    },
   },
   networks: {
-    goerli: {
-     
-        url: "https://eth-goerli.g.alchemy.com/v2/96b-b2u2vjIAiGGs4wX7n8Ac395pwsr4",
-      
-    }
+    hardhat: {
+      // uncomment to simulate executing tests on a mainnet fork
+      // forking: {
+      //   url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_PROVIDER}`, //"https://rpc.ankr.com/polygon"
+      // },
+      loggingEnabled: false,
     },
 
-  etherscan: {
-    apiKey: "G2NTBE18UF2M446DS5VV9AEFFSCXIKNV65",
+    mumbai: {
+      url: "https://rpc.ankr.com/polygon_mumbai",
+      chainId: 80001,
+      accounts: [process.env.PRIVATE_KEY1, process.env.PRIVATE_KEY2],
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
+      chainId: 5,
+  },
+  },
+
+  contractSizer: {
+    alphaSort: false,
+    disambiguatePaths: false,
+    runOnCompile: false,
+    strict: true,
+  },
+
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    coinmarketcap: process.env.COINMARKETCAP_APIKEY,
+    token: "MATIC",
+    gasPriceApi: `https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice&apiKey=${process.env.POLYGONSCAN_APIKEY}`,
   },
 };
-  // mocha: {
-  //   timeout: 100000000,
-  // },
+//BondERC1155
